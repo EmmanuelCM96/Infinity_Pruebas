67fd399e0a2ede5318332666e3977e042e82be87
Manuel <jesus120796cervantes@hotmail.com>
2022-08-01 11:48:09 -0500

    cambios_chicos

--- Infinity_prueba/Assets/Scripts/Procedural/Borrador.cs
+++ Infinity_prueba/Assets/Scripts/Procedural/Borrador.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Borrador : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private GameObject jugador;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        //jugador = GameObject.FindGameObjectWithTag("Borrar");
+    }
+    private void Update()
+    {
+        jugador = GameObject.FindGameObjectWithTag("Borrar");
+    }
+    // Start is called before the first frame update
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Borrar")
+        {
+            Debug.Log("otra vez medio funciona");
+            Destroy(jugador, .01f);
+        }
     }
 }
